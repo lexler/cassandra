5a4884d81ddecdbac9222ea312d09bcd3298580f
David Capwell <dcapwell@apache.org>
2024-07-26 12:42:34 -0700

    David review feedback

--- src/java/org/apache/cassandra/service/accord/serializers/CommandStoreSerializers.java
+++ src/java/org/apache/cassandra/service/accord/serializers/CommandStoreSerializers.java
@@ -129,7 +129,7 @@ public class CommandStoreSerializers
         }
     }), DurableBefore.Entry[]::new, DurableBefore.SerializerSupport::create);
 
-    public static IVersionedSerializer<RedundantBefore> redundantBefore = new ReducingRangeMapSerializer<>(NullableSerializer.wrap(new IVersionedSerializer<RedundantBefore.Entry>()
+    public static final IVersionedSerializer<RedundantBefore.Entry> redundantBeforeEntry = new IVersionedSerializer<>()
     {
         @Override
         public void serialize(RedundantBefore.Entry t, DataOutputPlus out, int version) throws IOException
@@ -152,7 +152,7 @@ public class CommandStoreSerializers
             long startEpoch = in.readUnsignedVInt();
             long endEpoch = in.readUnsignedVInt();
             if (endEpoch == 0) endEpoch = Long.MAX_VALUE;
-            else endEpoch = startEpoch + 1 + endEpoch;
+            else endEpoch = endEpoch - 1 + startEpoch;
             TxnId locallyAppliedOrInvalidatedBefore = CommandSerializers.txnId.deserialize(in, version);
             TxnId shardAppliedOrInvalidatedBefore = CommandSerializers.txnId.deserialize(in, version);
             TxnId bootstrappedAt = CommandSerializers.txnId.deserialize(in, version);
@@ -172,7 +172,8 @@ public class CommandStoreSerializers
             size += CommandSerializers.nullableTimestamp.serializedSize(t.staleUntilAtLeast, version);
             return size;
         }
-    }), RedundantBefore.Entry[]::new, RedundantBefore.SerializerSupport::create);
+    };
+    public static IVersionedSerializer<RedundantBefore> redundantBefore = new ReducingRangeMapSerializer<>(NullableSerializer.wrap(redundantBeforeEntry), RedundantBefore.Entry[]::new, RedundantBefore.SerializerSupport::create);
 
     private static class TimestampToRangesSerializer<T extends Timestamp> implements IVersionedSerializer<NavigableMap<T, Ranges>>
     {
--- test/unit/org/apache/cassandra/utils/AccordGenerators.java
+++ test/unit/org/apache/cassandra/utils/AccordGenerators.java
@@ -23,8 +23,11 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.function.BiFunction;
+import java.util.stream.Stream;
 
 import accord.local.Command;
+import accord.local.RedundantBefore;
 import accord.primitives.Deps;
 import accord.primitives.FullRoute;
 import accord.primitives.KeyDeps;
@@ -32,11 +35,14 @@ import accord.primitives.PartialTxn;
 import accord.primitives.Range;
 import accord.primitives.RangeDeps;
 import accord.primitives.Ranges;
+import accord.primitives.Routable;
 import accord.primitives.Timestamp;
+import accord.primitives.Txn;
 import accord.primitives.TxnId;
 import accord.utils.AccordGens;
 import accord.utils.Gen;
 import accord.utils.Gens;
+import accord.utils.RandomSource;
 import org.apache.cassandra.db.DecoratedKey;
 import org.apache.cassandra.dht.AccordSplitter;
 import org.apache.cassandra.dht.IPartitioner;
@@ -196,6 +202,19 @@ public class AccordGenerators
         return ranges(Gens.lists(fromQT(CassandraGenerators.TABLE_ID_GEN)).unique().ofSizeBetween(1, 10).map(l -> new HashSet<>(l)), ignore -> partitioner);
     }
 
+    public static Gen<Ranges> rangesArbitrary(IPartitioner partitioner)
+    {
+        Gen<Range> rangeGen = range(partitioner);
+        Gen.IntGen sizeGen = Gens.ints().between(0, 10);
+        return rs -> {
+            int targetSize = sizeGen.nextInt(rs);
+            List<Range> ranges = new ArrayList<>(targetSize);
+            for (int i = 0; i < targetSize; i++)
+                ranges.add(rangeGen.next(rs));
+            return Ranges.of(ranges.toArray(Range[]::new));
+        };
+    }
+
     public static Gen<KeyDeps> keyDepsGen()
     {
         return AccordGens.keyDeps(AccordGenerators.keys());
@@ -234,6 +253,35 @@ public class AccordGenerators
     public static Gen<Deps> depsGen(IPartitioner partitioner)
     {
         return AccordGens.deps(keyDepsGen(partitioner), rangeDepsGen(partitioner), directKeyDepsGen(partitioner));
+    }
+
+    public static Gen<RedundantBefore.Entry> redundantBeforeEntry(IPartitioner partitioner)
+    {
+        return redundantBeforeEntry(Gens.bools().all(), range(partitioner), AccordGens.txnIds(Gens.pick(Txn.Kind.SyncPoint, Txn.Kind.ExclusiveSyncPoint), ignore -> Routable.Domain.Range));
+    }
+
+    public static Gen<RedundantBefore.Entry> redundantBeforeEntry(Gen<Boolean> emptyGen, Gen<Range> rangeGen, Gen<TxnId> txnIdGen)
+    {
+        return rs -> {
+            Range range = rangeGen.next(rs);
+            TxnId locallyAppliedOrInvalidatedBefore = emptyGen.next(rs) ? TxnId.NONE : txnIdGen.next(rs); // emptyable or range
+            TxnId shardAppliedOrInvalidatedBefore = emptyGen.next(rs) ? TxnId.NONE : txnIdGen.next(rs); // emptyable or range
+            TxnId bootstrappedAt = txnIdGen.next(rs);
+            Timestamp staleUntilAtLeast = emptyGen.next(rs) ? null : txnIdGen.next(rs); // nullable
+
+            long maxEpoch = Stream.of(locallyAppliedOrInvalidatedBefore, shardAppliedOrInvalidatedBefore, bootstrappedAt, staleUntilAtLeast).filter(t -> t != null).mapToLong(Timestamp::epoch).max().getAsLong();
+            long startEpoch = rs.nextLong(maxEpoch);
+            long endEpoch = emptyGen.next(rs) ? Long.MAX_VALUE : 1 + rs.nextLong(startEpoch, Long.MAX_VALUE);
+            return new RedundantBefore.Entry(range, startEpoch, endEpoch, locallyAppliedOrInvalidatedBefore, shardAppliedOrInvalidatedBefore, bootstrappedAt, staleUntilAtLeast);
+        };
+    }
+
+    public static Gen<RedundantBefore> redundantBefore(IPartitioner partitioner)
+    {
+        Gen<Ranges> rangeGen = rangesArbitrary(partitioner);
+        Gen<TxnId> txnIdGen = AccordGens.txnIds(Gens.pick(Txn.Kind.SyncPoint, Txn.Kind.ExclusiveSyncPoint), ignore -> Routable.Domain.Range);
+        BiFunction<RandomSource, Range, RedundantBefore.Entry> entryGen = (rs, range) -> redundantBeforeEntry(Gens.bools().all(), i -> range, txnIdGen).next(rs);
+        return AccordGens.redundantBefore(rangeGen, entryGen);
     }
 
     public static <T> Gen<T> fromQT(org.quicktheories.core.Gen<T> qt)
